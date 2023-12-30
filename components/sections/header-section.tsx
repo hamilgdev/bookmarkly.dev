69346de79920700c60c181a79e4010d89341627d
hamilgdev <hamilgdev@gmail.com>
2023-12-30 17:26:13 -0500

    refactor: it removes the sticky

--- components/sections/header-section.tsx
+++ components/sections/header-section.tsx
@@ -2,7 +2,7 @@ import { Logo, ThemeDropdown } from '@/components'
 
 export const HeaderSection = () => {
   return (
-    <header className="sticky top-0 z-30 flex items-center min-h-[54px] bg-[var(--accent)] dark:bg-[var(--accent-v900)] drop-shadow-md border-b border-[var(--neutral-delicate)] dark:border-[var(--accent-v700)]">
+    <header className="flex items-center min-h-[54px]">
       <div className="l-container">
         <div className="flex items-center justify-between">
           <Logo />
